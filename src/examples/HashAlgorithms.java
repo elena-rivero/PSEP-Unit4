a72435b8da851ddb00aa45e3316a12cc734c56b8
elena-rivero <94249247+elena-rivero@users.noreply.github.com>
2023-02-15 20:17:12 +0100

    Creación de proyecto y clase para ver los algoritmos HASH disponibles

---
+++ src/examples/HashAlgorithms.java
@@ -0,0 +1,30 @@
+package examples;
+
+import java.security.MessageDigest;
+import java.security.Provider;
+import java.security.Provider.Service;
+import java.security.Security;
+import java.util.Set;
+
+public class HashAlgorithms {
+
+	public static void main(String[] args) {
+		// Se define el tipo de algoritmo como MessageDigest
+		final String TIPO_MESSAGE_DIGEST = MessageDigest.class.getSimpleName();
+	
+		// Se obtiene la lista de los proveedores de seguridad instalados
+		Provider[] proveedores = Security.getProviders();
+		
+		for(Provider proveedor : proveedores) {
+			// Por cada proveedor se obtiene el conjunto de servicios
+			Set<Service> servicios = proveedor.getServices();
+			for (Service servicio : servicios) {
+				// Se filtran los servicios por el tipo
+				if(servicio.getType().equals(TIPO_MESSAGE_DIGEST)) {
+					System.out.println(servicio.getAlgorithm());
+				}
+			}
+		}
+	}
+
+}

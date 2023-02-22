92c90b61577fcd7d47fad40424d035b718cfbd0d
elena-rivero <94249247+elena-rivero@users.noreply.github.com>
2023-02-22 18:04:34 +0100

    Ejemplo RSA completo

--- src/examples/rsa/KeysManager.java
+++ src/examples/rsa/KeysManager.java
@@ -1,44 +1,109 @@
 package examples.rsa;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.EncodedKeySpec;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 
 public class KeysManager {
 
 	private static final String PUBLIC_KEY_FILE = "public_key.key";
 	private static final String PRIVATE_KEY_FILE = "private_key.key";
-	
-	public static KeyPair generarClaves() throws NoSuchAlgorithmException{
-		KeyPairGenerator generador = KeyPairGenerator.getInstance("RSA");
-		generador.initialize(2048);
-		KeyPair claves = generador.generateKeyPair();
+
+	public static KeyPair generarClaves() {
+		KeyPairGenerator generador;
+		KeyPair claves = null;
+		try {
+			generador = KeyPairGenerator.getInstance("RSA");
+			generador.initialize(2048);
+			claves = generador.generateKeyPair();
+		} catch (NoSuchAlgorithmException e) {
+			System.err.println("No existe el algoritmo especificado");
+			e.printStackTrace();
+		}
+
 		return claves;
 	}
-	
+
 	public static void guardarClaves(KeyPair claves) {
 		FileOutputStream fos;
 		try {
 			fos = new FileOutputStream(PUBLIC_KEY_FILE);
 			fos.write(claves.getPublic().getEncoded());
 			fos.close();
-			
+
 			fos = new FileOutputStream(PRIVATE_KEY_FILE);
 			fos.write(claves.getPrivate().getEncoded());
 			fos.close();
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
+			System.err.println("No se encuentra el fichero.");
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			System.err.println("Se ha producido un error durante la escritura en el fichero.");
 			e.printStackTrace();
 		}
-		
-		
-		
+
 	}
-	
+
+	public static PublicKey getClavePublica() {
+		File ficheroClavePublica = new File(PUBLIC_KEY_FILE);
+		PublicKey clavePublica = null;
+		try {
+
+			byte[] bytesClavePublica = Files.readAllBytes(ficheroClavePublica.toPath());
+			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+			EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(bytesClavePublica);
+			clavePublica = keyFactory.generatePublic(publicKeySpec);
+
+		} catch (IOException e) {
+			System.err.println("Se ha producido en la lectura del fichero");
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			System.err.println("No existe el algoritmo especificado");
+			e.printStackTrace();
+		} catch (InvalidKeySpecException e) {
+			System.err.println("La clave indicada no es válida");
+			e.printStackTrace();
+		}
+		return clavePublica;
+	}
+
+	public static PrivateKey getClavePrivada() {
+		File ficheroClavePrivada = new File(PRIVATE_KEY_FILE);
+		PrivateKey clavePrivada = null;
+		try {
+
+			byte[] bytesClavePrivada = Files.readAllBytes(ficheroClavePrivada.toPath());
+			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+			EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(bytesClavePrivada);
+			clavePrivada = keyFactory.generatePrivate(privateKeySpec);
+
+		} catch (IOException e) {
+			System.err.println("Se ha producido en la lectura del fichero");
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			System.err.println("No existe el algoritmo especificado");
+			e.printStackTrace();
+		} catch (InvalidKeySpecException e) {
+			System.err.println("La clave indicada no es válida");
+			e.printStackTrace();
+		}
+		return clavePrivada;
+	}
+
+	public static void main(String[] args) {
+		KeyPair claves = generarClaves();
+		guardarClaves(claves);
+	}
 }
--- src/examples/rsa/RSADecryptExample.java
+++ src/examples/rsa/RSADecryptExample.java
@@ -19,24 +19,28 @@ import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 
-public class RSAEncryptExample {
-	public static final String MENSAJE = "Mensaje a cifrar";
+public class RSADecryptExample {
+	public static final String MENSAJE_CIFRADO = "Z2qKccwBQdEcgOdcBcA6rF4oeBHi52ifLUyAFakvx8iu5qo3LEb0TR2zYzQjwROdpt5M83Yt4eI8OW8gEpgTv0Z6nz+tzPBzVaWEoWW6EwKIGGP9ahUGg6IKWTy/tkytlme96qJWe7KSLIwkDQtKTVnVFsA1EonkTEZjegDyJsEYybRd4z1IYkkNRA2o0IIYrZoo8R8dN4ioNO9id5ztf69vDRVqXsc+YaoKwlVbZ+LC//3XS3Lxlox1vFCRMLp466bPvgXtv79W7au82Wl6nYWSPnTDRbIyx8gwauJ9GH6Q7+GCpT16rqFwvZ/nTKpkJUCInaBDxbrH9lpsvztQ6w==";
 	
 	public static void main(String[] args) {
 		
 		try {
-			// Ciframos con la clave pública
-			PublicKey clavePublica = KeysManager.getClavePublica();
+			// Tomamos la clave privada
+			PrivateKey clavePrivada = KeysManager.getClavePrivada();
 			
 			Cipher cipher = Cipher.getInstance("RSA");
-			cipher.init(Cipher.ENCRYPT_MODE, clavePublica);
 			
-			byte[] mensaje = MENSAJE.getBytes(StandardCharsets.UTF_8);
-			// Se cifra el mensaje
-			byte[] mensajeCifrado = cipher.doFinal(mensaje);
+			// Desciframos con la clave privada
+			cipher.init(Cipher.DECRYPT_MODE, clavePrivada);
+			
+			byte[] mensajeCifrado = Base64.getDecoder().decode(MENSAJE_CIFRADO);
+			
+			// Se obtiene el mensaje descifrado
+			byte[] mensaje = cipher.doFinal(mensajeCifrado);
 			
 			// Lo imprimimos por pantalla en Base64
-			System.out.println(Base64.getEncoder().encodeToString(mensajeCifrado));
+			System.out.println(new String(mensaje));
+			
 			
 		} catch (NoSuchAlgorithmException e) {
 			System.err.println("El algoritmo seleccionado no existe");
